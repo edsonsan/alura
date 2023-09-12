ba69fe2d91385a68777100800681e4b5cd63071c
EDSON RODRIGUES DOS SANTOS <erodrigues.santos@gmail.com>
2023-09-11 22:46:16 -0300

    Primeira Versão da Buscao

--- java/criandoapp/primeiroprojeto/src/PrincipalComBusca.java
+++ java/criandoapp/primeiroprojeto/src/PrincipalComBusca.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -19,6 +21,14 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
+
     }
 }
+
